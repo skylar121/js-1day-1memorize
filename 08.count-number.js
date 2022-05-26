c7404a6f869fe7e79ee11f14aa5c524c855b4aea
skylar121 <myway01212@naver.com>
2022-05-26 23:55:25 +0900

    5월 26일까지 공부

---
+++ 08.count-number.js
@@ -0,0 +1,8 @@
+// https://codingdojang.com/scode/393?langby=javascript#answer-filter-area
+// 1부터 10,000까지 8이라는 숫자가 총 몇번 나오는가?
+
+// 8이 포함되어 있는 숫자의 갯수를 카운팅 하는 것이 아니라 8이라는 숫자를 모두 카운팅 해야 한다.
+// (※ 예를들어 8808은 3, 8888은 4로 카운팅 해야 함)
+
+(Array(10000).fill(1).map((value, index)=>value+index)+'').split('8').length - 1;
+Array(10000).fill(1).map((value, index)=>value+index).join( "" ).match( /8/g ).length;
